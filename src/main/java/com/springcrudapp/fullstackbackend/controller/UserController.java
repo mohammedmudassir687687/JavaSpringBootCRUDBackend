6d1f4befec742aae263e06d08871c697c8d0a0b8
Mohammed Mudassir <mohammedmudassir687687@gmail.com>
2022-10-02 13:18:32 -0400

    Connecting react frontend to springboot backend using axios

--- src/main/java/com/springcrudapp/fullstackbackend/controller/UserController.java
+++ src/main/java/com/springcrudapp/fullstackbackend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.springcrudapp.fullstackbackend.controller;
 import com.springcrudapp.fullstackbackend.model.User;
 import com.springcrudapp.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
